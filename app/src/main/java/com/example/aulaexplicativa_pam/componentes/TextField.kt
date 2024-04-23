244672dcd02ca41ac71b5ec8ecf7e577cde14833
karolllopes <karolinna.lopes@etec.sp.gov.br>
2024-04-23 09:09:55 -0300

    TextField

---
+++ app/src/main/java/com/example/aulaexplicativa_pam/componentes/TextField.kt
@@ -0,0 +1,24 @@
+package com.example.aulaexplicativa_pam.componentes
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.setValue
+
+@Preview
+@Composable
+
+fun SimpleTextField () {
+
+    var text by remember { mutableStateOf("") }
+
+    TextField(value = text, onValueChange = {}, label = { Text(text= "Nome")})
+}
